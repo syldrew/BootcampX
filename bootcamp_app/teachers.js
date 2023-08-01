2685469a63f1747d2adceaf1c1e58ca614e2ab88
syldrew <syl_peke14@hotmail.com>
2023-08-01 09:48:35 -0500

    SQL Injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,17 +7,22 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
+
+const queryString = `SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+FROM assistance_requests
+JOIN students ON students.id = student_id
+JOIN cohorts ON students.cohort_id = cohorts.id 
+JOIN teachers ON teachers.id = teacher_id
+WHERE cohorts.name = $1
+GROUP BY teacher, cohort
+ORDER BY teacher;`;
+
+const values = [process.argv[2]];
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
   })
-});
+})
+.catch(err => console.error('query error', err.stack));
